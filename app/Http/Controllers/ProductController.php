b714baa68d853d40976e16f9ed0a710eb4317f52
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2020-01-02 14:12:31 +0700

    Lesson 3. Creating Routes, Methods, and Testing

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        dd('This is index');
+    }
+
+    public function store()
+    {
+        dd('This is store');
+    }
+}
